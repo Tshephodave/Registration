119f2ae7839dc302c48058fda0e97b9d0fab63bc
Tshephodave <111848064+Tshephodave@users.noreply.github.com>
2025-09-11 12:25:17 +0200

    Update registerRoutes.js

--- backend/routes/registerRoutes.js
+++ backend/routes/registerRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const { createRegister, getRegisters, getAllParticipants } = require('../controllers/registerController');
+const { createRegister, getAllParticipants, getLatestRegister } = require('../controllers/registerController');
 
 const router = express.Router();
 
 router.post('/', createRegister);
-router.get('/', getRegisters);
-router.get('/participants/all', getAllParticipants);  
-
+router.get('/latest', getLatestRegister); 
+router.get('/participants/all', getAllParticipants);
 
 module.exports = router;
